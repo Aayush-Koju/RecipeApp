3189b85166a1dc13cda19f7dbf35a8bbeea8ee8d
Aayush-Koju <iush.cozu12@gmail.com>
2025-06-19 12:22:20 +0545

    created RecipeCard Component

---
+++ src/components/RecipeCard.jsx
@@ -0,0 +1,11 @@
+export default function RecipeCard({ name, author, image }) {
+  return (
+    <div className="overflow-hidden hover:shadow-lg transition-shadow cursor-pointer">
+      <div className={`h-48 ${image}`}></div>
+      <div className="p-4">
+        <h3 className="font-semibold text-lg text-gray-800">{name}</h3>
+        <p className="text-gray-500 text-sm">{author}</p>
+      </div>
+    </div>
+  );
+}
